534b7ac0b7b50a87e8ade071e6feb0b302289ec7
Karl Ehrlich <contact@initech.de>
2025-10-26 17:24:10 +0100

    Refactored some scripts

--- scripts/create.sh
+++
@@ -1,6 +0,0 @@
-# create.sh
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-source venv/bin/activate
-pip install -r requirements.txt
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,15 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
+
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
 source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
 exec gunicorn -w 4 -b 0.0.0.0:8000 app:application \
   --pid gunicorn.pid \
   --access-logfile gunicorn-access.log \
